8d1a88f1e13677f830f41d60e55d16f2ad2aaa6f
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-26 15:54:53 -0600

    ¡Alejandro hizo otro cambio...!

--- Actividad9.py
+++ Actividad9.py
@@ -5,9 +5,9 @@ def agregar_pelicula(title, year, genre):
 
 def mostrar_peliculas():
     if len(peliculas) == 0:
-        print("No hay películas registradas")
+        print("¡No hay películas registradas!")
     else:
-        print("Películas registradas")
+        print("Películas registradas: ")
         for movie in peliculas:
             print(f"Título: {movie[0]}, Año: {movie[1]}, Género: {movie[2]}")
         print()
@@ -18,7 +18,7 @@ def buscar_por_genero(genero):
         if movie[2] == genero:
             encontrados.append(movie)
     if len(encontrados) > 0:
-        print("¡Películas encontradas!")
+        print("¡Película encontrada!")
         for movie in encontrados:
             print(f"Título: {movie[0]}, Año: {movie[1]}, Género: {movie[2]}")
     else:
@@ -71,10 +71,10 @@ while True:
     match option:
         case "1":
             quantity = int(input("¿Cuántas películas deseas agregar? "))
-            for _ in range(quantity):
-                title = input("Introduce el título: ")
-                year = int(input("Introduce el año: "))
-                genre = input("Introduce el género: ")
+            for i in range(quantity):
+                title = input(f"Introduce el título de la película {i+1}: ")
+                year = int(input(f"Introduce el año {i+1}: "))
+                genre = input(f"Introduce el género {i+1}: ")
                 agregar_pelicula(title, year, genre)
             print("¡Películas agregadas! \n")
         case "2":
